b2e49cc71cfe5f3ccb99553ec1fea2a0e787b445
Fernando Durban <ferbrizie@gmail.com>
2023-08-21 13:20:43 +0200

    cambiosfer

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import './Sidebar.css'
-import TwitterIcon from '@mui/icons-material/Twitter'
-import SidebarOption from '../SidebarOption/SidebarOption'
-import HomeIcon from '@mui/icons-material/Home'
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import SidebarOption from "../SidebarOption/SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
 
 const Sidebar = () => {
-    return (
-        <div className='sidebar'>
-            <TwitterIcon />
+  return (
+    <div className="sidebar">
+      <TwitterIcon />
 
-            <SidebarOption Icon={HomeIcon} text='inicio'/>
-            <SidebarOption/>
-            <SidebarOption/>
-            <SidebarOption/>
-            <SidebarOption/>
-            <SidebarOption/>
-            <SidebarOption/>
-            <SidebarOption/>
-            <SidebarOption/>
-        </div>
-    )
-}
+      <SidebarOption Icon={HomeIcon} text="inicio" />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
