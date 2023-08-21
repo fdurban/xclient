2ba821bbc34f92fbf4d2f36983e9becc128d0586
Fernando Durban <ferbrizie@gmail.com>
2023-08-21 22:04:15 +0200

    CAMBIOS FER

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import Sidebar from './components/Sidebar/Sidebar'
 function App() {
   return (
     <div className="App">
-      <h1>Twitter clon</h1>
 
       {/* sidebar */}
       <Sidebar/>
 
       {/* feed */}
-
+      <Feed/>
       {/* widgets */}
 
     </div>
--- src/components/SidebarOption/SidebarOption.jsx
+++ src/components/SidebarOption/SidebarOption.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import './SidebarOption.css'
 
-const SidebarOption = ({ text, Icon }) => {
+
+ const SidebarOption = ({ active, text, Icon }) => {
     return (
-        <>
+        <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
+
             <Icon />
-            <h2>
-                {text}
-            </h2>
-        </>
+
+            <h1>{text}</h1>
+        </div>
     )
 }
 
